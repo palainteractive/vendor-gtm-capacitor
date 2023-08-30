0760f05ebb162c06a3e2fa4f7b93bded027cb60e
Tomatpala <Tom.Sebastian@palainteractive.com>
2023-08-30 11:28:05 -0400

    Removed Files nand Updated Plugin

--- tes/dist/esm/index.js
+++ tes/dist/esm/index.js
@@ -1,12 +1,7 @@
 import { registerPlugin } from "@capacitor/core";
-import type { FirebaseAnalyticsPlugin } from "./definitions";
-
-const FirebaseAnalytics = registerPlugin<FirebaseAnalyticsPlugin>(
-  "FirebaseAnalytics",
-  {
+const FirebaseAnalytics = registerPlugin("FirebaseAnalytics", {
     web: () => import("./web").then((m) => new m.FirebaseAnalyticsWeb()),
-  }
-);
-
+});
 export * from "./definitions";
 export { FirebaseAnalytics };
+//# sourceMappingURL=index.js.map
